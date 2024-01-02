e8796eb3c4a29139b4f7459a732e6ebda633c922
Kaveendra Randeera <khkrandeera@gmail.com>
2024-01-02 10:40:41 +0530

    Finish the final project

--- src/main/java/lk/ijse/dep11/edupanel/api/LecturerHttpController.java
+++ src/main/java/lk/ijse/dep11/edupanel/api/LecturerHttpController.java
@@ -36,7 +36,7 @@ public class LecturerHttpController {
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(consumes = "multipart/form-data", produces = "application/json")
     public LecturerTO createNewLecturer(@ModelAttribute @Validated(LecturerReqTO.Create.class)
-                                      LecturerReqTO lecturerReqTo){
+                                        LecturerReqTO lecturerReqTo) {
         em.getTransaction().begin();
         try {
             Lecturer lecturer = mapper.map(lecturerReqTo, Lecturer.class);
@@ -45,13 +45,13 @@ public class LecturerHttpController {
             em.persist(lecturer);
             LecturerTO lecturerTO = mapper.map(lecturer, LecturerTO.class);
 
-            if (lecturerReqTo.getLinkedin() != null){
+            if (lecturerReqTo.getLinkedin() != null) {
                 em.persist(new LinkedIn(lecturer, lecturerReqTo.getLinkedin()));
                 lecturerTO.setLinkedin(lecturerReqTo.getLinkedin());
             }
 
-            if (lecturerReqTo.getPicture() != null){
-                Picture picture = new Picture(lecturer, "lectures/" + lecturer.getId());
+            if (lecturerReqTo.getPicture() != null) {
+                Picture picture = new Picture(lecturer, "lecturers/" + lecturer.getId());
                 em.persist(picture);
 
                 Blob blobRef = bucket.create(picture.getPicturePath(), lecturerReqTo.getPicture().getInputStream(), lecturerReqTo.getPicture().getContentType());
@@ -60,7 +60,7 @@ public class LecturerHttpController {
 
             em.getTransaction().commit();
             return lecturerTO;
-        }catch (Throwable t){
+        } catch (Throwable t) {
             em.getTransaction().rollback();
             throw new RuntimeException(t);
         }
@@ -68,53 +68,111 @@ public class LecturerHttpController {
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PatchMapping(value = "/{lecturer-id}", consumes = "multipart/form-data")
-    public void updateLecturerDetailsViaMultipart(@PathVariable("lecturer-id") Integer lecturerId){}
+    public void updateLecturerDetailsViaMultipart(@PathVariable("lecturer-id") Integer lecturerId,
+                                                  @ModelAttribute @Validated(LecturerReqTO.Update.class) LecturerReqTO lecturerReqTO) {
+        Lecturer currentLecturer = em.find(Lecturer.class, lecturerId);
+        if (currentLecturer == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+        em.getTransaction().begin();
+        try {
+            Lecturer newLecturer = mapper.map(lecturerReqTO, Lecturer.class);
+            newLecturer.setId(lecturerId);
+            newLecturer.setPicture(null);
+            newLecturer.setLinkedIn(null);
+
+            if (lecturerReqTO.getPicture() != null) {
+                newLecturer.setPicture(new Picture(newLecturer, "lecturers/" + lecturerId));
+            }
+            if (lecturerReqTO.getLinkedin() != null) {
+                newLecturer.setLinkedIn(new LinkedIn(newLecturer, lecturerReqTO.getLinkedin()));
+            }
+
+           updateLinkedIn(currentLecturer, newLecturer);
+
+            if (newLecturer.getPicture() != null && currentLecturer.getPicture() == null) {
+                em.persist(newLecturer.getPicture());
+                bucket.create(newLecturer.getPicture().getPicturePath(), lecturerReqTO.getPicture().getInputStream(), lecturerReqTO.getPicture().getContentType());
+            } else if (newLecturer.getPicture() == null && currentLecturer.getPicture() != null) {
+                em.remove(currentLecturer.getPicture());
+                bucket.get(currentLecturer.getPicture().getPicturePath()).delete();
+            } else if (newLecturer.getPicture() != null) {
+                em.merge(newLecturer.getPicture());
+                bucket.create(newLecturer.getPicture().getPicturePath(), lecturerReqTO.getPicture().getInputStream(), lecturerReqTO.getPicture().getContentType());
+            }
+
+            em.merge(newLecturer);
+            em.getTransaction().commit();
+        } catch (Throwable t) {
+            em.getTransaction().rollback();
+            throw new RuntimeException(t);
+        }
+    }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PatchMapping(value = "/{lecturer-id}", consumes = "application/json")
-    public void updateLecturerDetailsViaJson(@PathVariable("lecturer-id") Integer lecturerId){}
+    public void updateLecturerDetailsViaJson(@PathVariable("lecturer-id") Integer lecturerId,
+                                             @RequestBody @Validated LecturerTO lecturerTO) {
+        Lecturer currentLecturer = em.find(Lecturer.class, lecturerId);
+        if (currentLecturer == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+        em.getTransaction().begin();
+        try {
+            Lecturer newLecturer = mapper.map(lecturerTO, Lecturer.class);
+            newLecturer.setId(lecturerId);
+            newLecturer.setPicture(currentLecturer.getPicture());
+            newLecturer.setLinkedIn(lecturerTO.getLinkedin() != null ? new LinkedIn(newLecturer, lecturerTO.getLinkedin()) : null);
+
+            updateLinkedIn(currentLecturer, newLecturer);
+
+            em.merge(newLecturer);
+            em.getTransaction().commit();
+        } catch (Throwable t) {
+            em.getTransaction().rollback();
+            throw new RuntimeException(t);
+        }
+    }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{lecturer-id}")
-    public void deleteLecturer(@PathVariable("lecturer-id") Integer lecturerId){
+    public void deleteLecturer(@PathVariable("lecturer-id") Integer lecturerId) {
         Lecturer lecturer = em.find(Lecturer.class, lecturerId);
         if (lecturer == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         em.getTransaction().begin();
         try {
             em.remove(lecturer);
 
-            if (lecturer.getPicture() != null){
+            if (lecturer.getPicture() != null) {
                 bucket.get(lecturer.getPicture().getPicturePath()).delete();
             }
 
             em.getTransaction().commit();
-        }catch (Throwable t){
+        } catch (Throwable t) {
             em.getTransaction().rollback();
             throw new RuntimeException(t);
         }
     }
 
     @GetMapping(produces = "application/json")
-    public List<LecturerTO> getAllLecturers(){
+    public List<LecturerTO> getAllLecturers() {
         TypedQuery<Lecturer> query = em.createQuery("SELECT l FROM Lecturer l", Lecturer.class);
         return getLecturerTOList(query);
     }
 
     @GetMapping(value = "/{lecturer-id}", produces = "application/json")
-    public LecturerTO getLecturerDetails(@PathVariable("lecturer-id") Integer lecturerId){
+    public LecturerTO getLecturerDetails(@PathVariable("lecturer-id") Integer lecturerId) {
         Lecturer lecturer = em.find(Lecturer.class, lecturerId);
         if (lecturer == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         return getLecturerTO(lecturer);
     }
 
     @GetMapping(params = "type=full-time", produces = "application/json")
-    public List<LecturerTO> getFullTimeLecturers(){
+    public List<LecturerTO> getFullTimeLecturers() {
         TypedQuery<Lecturer> query = em.createQuery("SELECT l FROM Lecturer l WHERE l.type = lk.ijse.dep11.edupanel.util.LecturerType.FULL_TIME", Lecturer.class);
         return getLecturerTOList(query);
     }
 
     @GetMapping(params = "type=visiting", produces = "application/json")
-    public List<LecturerTO> getVisitingLecturers(){
+    public List<LecturerTO> getVisitingLecturers() {
         TypedQuery<Lecturer> query = em.createQuery("SELECT l FROM Lecturer l WHERE l.type = lk.ijse.dep11.edupanel.util.LecturerType.VISITING", Lecturer.class);
         return getLecturerTOList(query);
     }
@@ -126,9 +184,19 @@ public class LecturerHttpController {
     private LecturerTO getLecturerTO(Lecturer lectureEntity) {
         LecturerTO lecturerTO = mapper.map(lectureEntity, LecturerTO.class);
         if (lectureEntity.getLinkedIn() != null) lecturerTO.setLinkedin(lectureEntity.getLinkedIn().getUrl());
-        if (lectureEntity.getPicture() != null){
+        if (lectureEntity.getPicture() != null) {
             lecturerTO.setPicture(bucket.get(lectureEntity.getPicture().getPicturePath()).signUrl(1, TimeUnit.DAYS, Storage.SignUrlOption.withV4Signature()).toString());
         }
         return lecturerTO;
+    }
+
+    private void updateLinkedIn(Lecturer currentLecturer, Lecturer newLecturer){
+        if (newLecturer.getLinkedIn() != null && currentLecturer.getLinkedIn() == null) {
+            em.persist(newLecturer.getLinkedIn());
+        } else if (newLecturer.getLinkedIn() == null && currentLecturer.getLinkedIn() != null) {
+            em.remove(currentLecturer.getLinkedIn());
+        } else if (newLecturer.getLinkedIn() != null) {
+            em.merge(newLecturer.getLinkedIn());
+        }
     }
 }
--- src/main/java/lk/ijse/dep11/edupanel/to/LecturerTO.java
+++ src/main/java/lk/ijse/dep11/edupanel/to/LecturerTO.java
@@ -4,19 +4,33 @@ import lk.ijse.dep11.edupanel.util.LecturerType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.*;
 import java.io.Serializable;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class LecturerTO implements Serializable {
+    @Null(message = "Id should be empty")
     private Integer id;
+    @NotBlank(message = "Name can't be empty")
+    @Pattern(regexp = "^[A-Za-z ]{2,}$", message = "Invalid name")
     private String name;
+    @NotBlank(message = "Designation can't be empty")
+    @Length(min = 3, message = "Invalid designation")
     private String designation;
+    @NotBlank(message = "Qualifications can't be empty")
+    @Length(min = 3, message = "Invalid qualifications")
     private String qualifications;
+    @NotNull(message = "Type should be either full-time or visiting")
     private LecturerType type;
+    @NotNull(message = "Display order can't be empty")
+    @PositiveOrZero(message = "Invalid display order")
     private Integer displayOrder;
+    @Null(message = "Picture should be empty")
     private String picture;
+    @Pattern(regexp = "^http(s)://.+$", message = "Invalid linkedin url")
     private String linkedin;
 }
