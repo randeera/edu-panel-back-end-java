06fa37fc495b4039c4f46797bc96a01e3940d637
Kaveendra Randeera <khkrandeera@gmail.com>
2024-01-04 17:55:56 +0530

    Add transformer 1st test case

---
+++ src/test/java/lk/ijse/dep11/edupanel/service/util/TransformerTest.java
@@ -0,0 +1,38 @@
+package lk.ijse.dep11.edupanel.service.util;
+
+import lk.ijse.dep11.edupanel.WebAppConfig;
+import lk.ijse.dep11.edupanel.WebRootConfig;
+import lk.ijse.dep11.edupanel.entity.Lecturer;
+import lk.ijse.dep11.edupanel.entity.LinkedIn;
+import lk.ijse.dep11.edupanel.to.LecturerTO;
+import lk.ijse.dep11.edupanel.util.LecturerType;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringJUnitWebConfig(classes = {WebAppConfig.class, WebRootConfig.class})
+class TransformerTest {
+
+    private final Transformer transformer = new Transformer();
+
+    @Test
+    void toLecturerTO() {
+        Lecturer lecturer = new Lecturer(2,
+                "Nuwan",
+                "Associate Trainer",
+                "BSc in Computing",
+                LecturerType.VISITING,
+                5);
+        lecturer.setLinkedIn(new LinkedIn(lecturer, "https://linkedin.com/nuwan"));
+        LecturerTO lecturerTO = transformer.toLecturerTO(lecturer);
+
+        assertEquals(lecturer.getId(), lecturerTO.getId());
+        assertEquals(lecturer.getName(), lecturerTO.getName());
+        assertEquals(lecturer.getDesignation(), lecturerTO.getDesignation());
+        assertEquals(lecturer.getQualifications(), lecturerTO.getQualifications());
+        assertEquals(lecturer.getType(), lecturerTO.getType());
+        assertEquals(lecturer.getLinkedIn().getUrl(), lecturerTO.getLinkedin());
+    }
+}
