7bad94f786a2c5ba8e7823e580dc54ca4f296c5c
Kaveendra Randeera <khkrandeera@gmail.com>
2024-01-04 17:04:19 +0530

    Implement LecturerService

--- src/main/java/lk/ijse/dep11/edupanel/service/custom/LecturerService.java
+++ src/main/java/lk/ijse/dep11/edupanel/service/custom/LecturerService.java
@@ -1,6 +1,24 @@
 package lk.ijse.dep11.edupanel.service.custom;
 
 import lk.ijse.dep11.edupanel.service.SuperService;
+import lk.ijse.dep11.edupanel.to.LecturerTO;
+import lk.ijse.dep11.edupanel.to.request.LecturerReqTO;
+import lk.ijse.dep11.edupanel.util.LecturerType;
+import org.w3c.dom.stylesheets.LinkStyle;
+
+import java.util.List;
 
 public interface LecturerService extends SuperService {
+
+    LecturerTO saveLecturer(LecturerReqTO lecturerReqTO);
+
+    void updateLecturerDetailsWithImage(LecturerReqTO lecturerReqTO);
+
+    void updateLecturerDetailsWithoutImage(LecturerTO lecturerTO);
+
+    void deleteLecturer(Integer lecturerId);
+
+    LecturerTO getLecturerDetails(Integer lecturerId);
+
+    List<LecturerTO> getLecturers(LecturerType type);
 }
