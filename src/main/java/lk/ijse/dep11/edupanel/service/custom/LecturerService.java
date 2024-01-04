c766881a2027effc0c7d8667ddf6741bfec14538
Kaveendra Randeera <khkrandeera@gmail.com>
2024-01-04 16:47:57 +0530

    Finalize the structure of the service layer

---
+++ src/main/java/lk/ijse/dep11/edupanel/service/custom/LecturerService.java
@@ -0,0 +1,6 @@
+package lk.ijse.dep11.edupanel.service.custom;
+
+import lk.ijse.dep11.edupanel.service.SuperService;
+
+public interface LecturerService extends SuperService {
+}
---
+++ src/main/java/lk/ijse/dep11/edupanel/service/custom/impl/LecturerServiceImpl.java
@@ -0,0 +1,6 @@
+package lk.ijse.dep11.edupanel.service.custom.impl;
+
+import lk.ijse.dep11.edupanel.service.custom.LecturerService;
+
+public class LecturerServiceImpl implements LecturerService {
+}
