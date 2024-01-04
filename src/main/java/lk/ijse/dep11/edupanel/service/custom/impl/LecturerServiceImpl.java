f19c3ff1f18f8625699b083d0d1392cd86d2e86d
Kaveendra Randeera <khkrandeera@gmail.com>
2024-01-04 17:17:31 +0530

    Add AppStore

--- src/main/java/lk/ijse/dep11/edupanel/service/custom/impl/LecturerServiceImpl.java
+++ src/main/java/lk/ijse/dep11/edupanel/service/custom/impl/LecturerServiceImpl.java
@@ -1,6 +1,52 @@
 package lk.ijse.dep11.edupanel.service.custom.impl;
 
+import lk.ijse.dep11.edupanel.repository.RepositoryFactory;
+import lk.ijse.dep11.edupanel.repository.custom.LecturerRepository;
+import lk.ijse.dep11.edupanel.repository.custom.LinkedInRepository;
+import lk.ijse.dep11.edupanel.repository.custom.PictureRepository;
 import lk.ijse.dep11.edupanel.service.custom.LecturerService;
+import lk.ijse.dep11.edupanel.to.LecturerTO;
+import lk.ijse.dep11.edupanel.to.request.LecturerReqTO;
+import lk.ijse.dep11.edupanel.util.LecturerType;
+
+import java.util.List;
 
 public class LecturerServiceImpl implements LecturerService {
+
+    private final LecturerRepository lecturerRepository = RepositoryFactory.getInstance()
+            .getRepository(RepositoryFactory.RepositoryType.LECTURER);
+    private final LinkedInRepository linkedInRepository = RepositoryFactory.getInstance()
+            .getRepository(RepositoryFactory.RepositoryType.LINKEDIN);
+    private final PictureRepository pictureRepository = RepositoryFactory.getInstance()
+            .getRepository(RepositoryFactory.RepositoryType.PICTURE);
+
+    @Override
+    public LecturerTO saveLecturer(LecturerReqTO lecturerReqTO) {
+        return null;
+    }
+
+    @Override
+    public void updateLecturerDetailsWithImage(LecturerReqTO lecturerReqTO) {
+
+    }
+
+    @Override
+    public void updateLecturerDetailsWithoutImage(LecturerTO lecturerTO) {
+
+    }
+
+    @Override
+    public void deleteLecturer(Integer lecturerId) {
+
+    }
+
+    @Override
+    public LecturerTO getLecturerDetails(Integer lecturerId) {
+        return null;
+    }
+
+    @Override
+    public List<LecturerTO> getLecturers(LecturerType type) {
+        return null;
+    }
 }
---
+++ src/main/java/lk/ijse/dep11/edupanel/store/AppStore.java
@@ -0,0 +1,28 @@
+package lk.ijse.dep11.edupanel.store;
+
+import com.google.cloud.storage.Bucket;
+
+import javax.persistence.EntityManager;
+
+public class AppStore {
+
+    private static final ThreadLocal<EntityManager> emStore = new ThreadLocal<>();
+    private static final ThreadLocal<Bucket> bucketStore = new ThreadLocal<>();
+
+    public static EntityManager getEntityManager(){
+        return emStore.get();
+    }
+
+    public static Bucket getBucket(){
+        return bucketStore.get();
+    }
+
+    public static void setEntityManager(EntityManager entityManager){
+        emStore.set(entityManager);
+    }
+
+    public static void setBucket(Bucket bucket){
+        bucketStore.set(bucket);
+    }
+
+}
