2f6058d112736b5138b6a6d169236b201b140422
Kaveendra Randeera <khkrandeera@gmail.com>
2024-01-04 13:54:51 +0530

    Update PictureRepositoryImpl

--- src/main/java/lk/ijse/dep11/edupanel/repository/custom/impl/PictureRepositoryImpl.java
+++ src/main/java/lk/ijse/dep11/edupanel/repository/custom/impl/PictureRepositoryImpl.java
@@ -1,9 +1,22 @@
 package lk.ijse.dep11.edupanel.repository.custom.impl;
 
 import lk.ijse.dep11.edupanel.entity.Lecturer;
+import lk.ijse.dep11.edupanel.entity.LinkedIn;
 import lk.ijse.dep11.edupanel.entity.Picture;
 import lk.ijse.dep11.edupanel.repository.CrudRepositoryImpl;
 import lk.ijse.dep11.edupanel.repository.custom.PictureRepository;
 
+import java.util.Optional;
+
 public class PictureRepositoryImpl extends CrudRepositoryImpl<Picture, Lecturer> implements PictureRepository {
+
+    @Override
+    public void deleteById(Lecturer pk) {
+        getEntityManager().remove(getEntityManager().find(Picture.class, pk.getId()));
+    }
+
+    @Override
+    public Optional<Picture> findById(Lecturer pk) {
+        return Optional.ofNullable(getEntityManager().find(Picture.class, pk.getId()));
+    }
 }
